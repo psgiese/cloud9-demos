2d65a832e92c1dbe2735a22545db82061ac7b4b2
EC2 Default User <ec2-user@ip-172-31-58-162.ec2.internal>
2022-07-31 02:34:44 +0000

    rewriting to lint

--- daily_coding_problems/ciscobitswap.py
+++ daily_coding_problems/ciscobitswap.py
@@ -1,21 +1,10 @@
-import pandas as pd
-import numpy as np
-
-## Given an unsigned 8-bit integer, swap its even and odd bits. The 1st and 2nd bit should be swapped, the 3rd and 4th bit should be swapped, and so on.
-
-### For example, 10101010 should be 01010101
-
 def bit_swap(x):
+    """ this is a function that helps you swap bit positions """
     start = list(str(x))
-    middle = list(zip(np.arange(len(start)), start))
-    #print(middle)
     end = []
-    for idx,int_ in middle:
+    for idx in range(len(start)):
         if idx%2==0:
-            end.append(middle[idx+1])
-        else: end.append(middle[idx-1])
-    middle_2 = [j for i,j in end]
-    
-    return int(''.join(map(str,middle_2)))
-    
+            end.append(start[idx+1])
+        else: end.append(start[idx-1])
+    return int(''.join(map(str,end)))
 print((bit_swap(1234)))
---
+++ daily_coding_problems/test_dcc.py
@@ -0,0 +1,4 @@
+from ciscobitswap import bit_swap
+
+def test_dcc():
+    assert 2143 == bit_swap(1234)
