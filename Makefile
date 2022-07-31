b02e6b6f158665e8d3843bb46e95731a87bc6301
EC2 Default User <ec2-user@ip-172-31-51-249.ec2.internal>
2022-07-31 22:12:36 +0000

    adding black back

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ install:
 lint:
 	pylint --disable=R,C hello.py ##disable recommended & config warnings, keeps warnings and errors
 
-##format:
-##	black -
+format:
+	black *.py
 
 test:
 	python -m pytest -vv --cov=hello test_hello.py
--- daily_coding_problems/tests/ciscobitswap.py
+++ daily_coding_problems/tests/ciscobitswap.py
@@ -3,8 +3,8 @@ def bit_swap(x):
     start = list(str(x))
     end = []
     for idx in range(len(start)):
-        if idx%2==0:
+        if idx%2 == 0:
             end.append(start[idx+1])
         else: end.append(start[idx-1])
-    return int(''.join(map(str,end)))
-print((bit_swap(1234)))
+    return int(''.join(map(str, end)))
+print(bit_swap(1234))
